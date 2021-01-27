d684738fe339a8c2ee695d4d1e51a3883710c86b
Wissam <wissam_ataleh@outlook.com>
2021-01-27 14:47:48 +0200

    Add i18n module and config

---
+++ lang/en.js
@@ -0,0 +1,3 @@
+export default {
+    title: "Digital Safety Portal - JOSA"
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -36,10 +36,39 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'nuxt-i18n',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
+
+  // I18n conmodule configurationfig
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        iso: 'en',
+        file: 'en.js',
+        name: 'English',
+        dir: 'ltr'
+      },
+      {
+        code: 'ar',
+        iso: 'ar',
+        file: 'ar.js',
+        name: 'العربية',
+        dir: 'rtl'
+      }
+    ],
+    defaultLocale: 'en',
+    vueI18n: {
+      fallbackLocale: 'en',
+    },
+    lazy: true,
+    langDir: 'lang/',
+    detectBrowserLanguage: false,
+    seo: true
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
