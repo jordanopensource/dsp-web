c5fe694ed8b69ee17a1f341fa8f440d335a4c575
Wissam <wissam_ataleh@outlook.com>
2021-01-29 06:27:45 +0200

    Add component UI/Image

---
+++ components/UI/Image.vue
@@ -0,0 +1,39 @@
+<template>
+  <img :src="imageUrl" :alt="image.alternativeText">
+</template>
+
+<script>
+  export default {
+    props: {
+      image: {
+        type: Object,
+        required: true
+      },
+      size: {
+        type: String
+      }
+    },
+    computed: {
+      imageUrl() {
+        try {
+          return process.env.baseUrl + this.format().url
+        } catch {
+          return process.env.baseUrl + this.image.url
+        }
+      }
+    },
+    methods: {
+      format() {
+        const size = this.size;
+        const format = this.image.formats[size];
+        if (format) {
+          return format
+        } else {
+          const formats = this.image.formats;
+          const first = Object.keys(formats)[0]
+          return formats[first]
+        }
+      }
+    }
+  }
+</script>
