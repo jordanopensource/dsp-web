03ec988a0e2911b9834e6e714e63228ef9c56952
Wissam <wissam_ataleh@outlook.com>
2021-01-29 20:19:15 +0200

    Add component UI/FilterMenu

---
+++ components/UI/FilterMenu.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="py-6 container">
+    <div class="flex flex-col md:flex-row">
+      <span :class="active == 'all' ? 'active': ''" class="link" @click="setActive('all')"><h3>{{ $t('all') }}</h3></span>
+      <span v-for="item in items" :key="item.id" :class="active == item.name ? 'active': ''" class="link"
+        @click="setActive(item.name)"><h3>{{ item['title_' + $i18n.locale] }}</h3></span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      items: {
+        type: Array,
+        required: true
+      },
+      active: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      setActive(value) {
+        this.$emit('setActive', value)
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .link {
+    @apply cursor-pointer p-2;
+  }
+
+  .link:hover,
+  .link.active {
+    @apply rounded-lg;
+    background-color: #c3d4e0;
+  }
+
+  @screen md {
+    [dir="ltr"] .link {
+      @apply mr-4;
+    }
+
+    [dir="rtl"] .link {
+      @apply ml-4;
+    }
+  }
+
+</style>
