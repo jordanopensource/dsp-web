a8f17f6d88c6ca71338dbf7cf80f0fb384ff0446
Wissam <wissam_ataleh@outlook.com>
2021-01-29 20:20:02 +0200

    Add guides-categories to guides store

--- store/guides.js
+++ store/guides.js
@@ -1,19 +1,28 @@
 import axios from 'axios';
 
 export const state = () => ({
-  list: []
+  list: [],
+  categories: []
 })
 
 export const mutations = {
   set(state, content) {
     state.list = content
+  },
+  setCategories(state, content) {
+    state.categories = content
   }
 }
 
 export const actions = {
-    async fetch({ commit }) {
+  async fetch({commit}) {
     const response = await axios.get(this.$config.APIBaseURL + '/guides');
     const content = response.data;
     commit("set", content);
+  },
+  async fetchCategories({commit}) {
+    const response = await axios.get(this.$config.APIBaseURL + '/guide-categories');
+    const content = response.data;
+    commit("setCategories", content);
   }
 }
