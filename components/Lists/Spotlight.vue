15d96216c35193ba4508bb6fff75708e7dc10cb8
Wissam <wissam_ataleh@outlook.com>
2021-01-29 06:26:02 +0200

    Add component Lists/Spotlight

---
+++ components/Lists/Spotlight.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="spotlight-list container">
+    <h2 class="my-4">{{ title }}</h2>
+    <ViewsFull
+        class="mb-6"
+        :id="content.id" 
+        :image="content.image" 
+        :title="content['title_' + $i18n.locale]"
+        :description="content['description_' + $i18n.locale]"
+        :dateCreated="content.created_at"
+        :dateUpdated="content.updated_at"
+        :contributor="content.contributors[0]" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      content: {
+        type: Object,
+        required: true
+      },
+    }
+  }
+
+</script>
