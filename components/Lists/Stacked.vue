fe8956ad3e4acc0f9ca38cf7cb1ee9d301e0c5e7
Wissam <wissam_ataleh@outlook.com>
2021-01-29 18:16:08 +0200

    Add component Lists/Stacked

---
+++ components/Lists/Stacked.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="stacked-list container">
+    <h2 class="my-4">{{ title }}</h2>
+    <ViewsCompact
+        v-for="item in contentList"
+        :key="item.id"
+        class="mb-6"
+        :id="item.id" 
+        :title="item['title_' + $i18n.locale]"
+        :dateCreated="item.created_at"
+        :dateUpdated="item.updated_at"
+        :link="localePath('/guides/' + item.id)" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      contentList: {
+        type: Array,
+        required: true
+      },
+    }
+  }
+
+</script>
