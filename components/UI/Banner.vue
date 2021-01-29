f70707ea8abf255f182b3a559c64422673f880d5
Wissam <wissam_ataleh@outlook.com>
2021-01-29 06:27:23 +0200

    Add component UI/Banner

---
+++ components/UI/Banner.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="banner" class="bg-white">
+    <div class="container">
+      <div class="text-left py-20">
+        <h1 class="mb-8">{{ title }}</h1>
+        <h3>{{ description }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      }
+    }
+  }
+
+</script>
