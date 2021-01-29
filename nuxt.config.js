5b34517c0d84de4c1dce953ca8efd9c7131dc3da
Wissam <wissam_ataleh@outlook.com>
2021-01-29 06:14:14 +0200

    Add nuxtjs/moment and datetime-filters plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/i18n' },
+    { src: '~/plugins/datetime-filter' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -43,6 +44,7 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     'nuxt-i18n',
+    '@nuxtjs/moment',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
