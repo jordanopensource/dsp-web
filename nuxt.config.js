f206608d056a71b5d5644ddd5e1a5f762a357b96
Wissam <wissam_ataleh@outlook.com>
2021-01-29 06:28:33 +0200

    Add components auto import and prefixing configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,12 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    '~/components/',
+    { path: '~/components/UI/', prefix: 'UI' },
+    { path: '~/components/Lists/', prefix: 'Lists' },
+    { path: '~/components/Views/', prefix: 'Views' },
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
