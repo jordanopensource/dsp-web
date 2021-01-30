805870e25e076b93d4a84eb454abf1bf358e50ab
Wissam <wissam_ataleh@outlook.com>
2021-01-31 01:52:12 +0200

    Update spacing

--- components/Views/Meduim.vue
+++ components/Views/Meduim.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="view-full p-8 bg-white rounded-lg">
-    <NuxtLink :to="link" class="block w-full">
+  <div class="view-full p-4 bg-white rounded-lg flex flex-row md:flex-col flex-no-wrap">
+    <NuxtLink :to="link" class="block w-2/5 md:w-full flex-shrink-0">
       <UIImage v-if="image" :image="image" size="medium" class="image" />
       <img v-else src="/images/placeholder.png" alt="Placeholder" class="image" />
     </NuxtLink>
-    <NuxtLink :to="link">
-      <h3 class="my-6">{{ title }}</h3>
-    </NuxtLink>
-    <p>{{ dateUpdated | fullDate($i18n.locale) }} / {{contributor['name_' + $i18n.locale]}}</p>
+    <div class="ltr:ml-8 rtl:mr-8 md:ltr:ml-0 md:rtl:mr-0 md:mt-4 w-full">
+      <NuxtLink :to="link">
+        <h3 class="mb-4">{{ title }}</h3>
+      </NuxtLink>
+      <p>{{ dateUpdated | fullDate($i18n.locale) }} / {{contributor['name_' + $i18n.locale]}}</p>
+    </div>
   </div>
 </template>
 
