1643e2becf67098de4dc9588907056921f1b5cf8
Wissam <wissam_ataleh@outlook.com>
2021-01-30 17:33:48 +0200

    Add component Sliders/Guide

---
+++ components/Sliders/Guides.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="slider bg-white">
+    <div class="container py-16">
+      <client-only placeholder="Loading...">
+        <agile :options="sliderOption">
+          <SlideTypesGuide v-for="slide in slider.slides[0].Slide" :key="slide.id" :id='"slide-" + slide.id'
+            :slide="slide" class="slide" />
+          <template slot="prevButton">Prev</template>
+          <template slot="nextButton">Next</template>
+        </agile>
+      </client-only>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        sliderOption: {
+          navButtons: true,
+          autoplay: true,
+          fade: true,
+          infinite: true,
+          pauseOnHover: true,
+          dots: false
+        }
+      }
+    },
+    props: {
+      slider: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+
+</script>
+
+<style>
+  .agile__actions {
+    @apply block;
+  }
+
+  [dir='ltr'] .agile__actions {
+    @apply text-right;
+  }
+
+  [dir="rtl"] .agile__actions {
+    @apply text-left;
+  }
+
+  .agile__nav-button {
+    @apply text-josa-grey;
+  }
+
+  .agile__nav-button:hover {
+    @apply text-josa-grey-dark
+  }
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,7 @@ export default {
     { path: '~/components/UI/', prefix: 'UI' },
     { path: '~/components/Lists/', prefix: 'Lists' },
     { path: '~/components/Views/', prefix: 'Views' },
+    { path: '~/components/Sliders/', prefix: 'Sliders' },
     { path: '~/components/SlideTypes/', prefix: 'SlideTypes' },
   ],
 
