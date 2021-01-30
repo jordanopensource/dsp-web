249b33d3f5d9ca3f4f82c60edbf23c716fce0929
Wissam <wissam_ataleh@outlook.com>
2021-01-30 20:03:44 +0200

    Add component Lists/RecomSpotlight

---
+++ components/Lists/RecomSpotlight.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="spotlight-list container">
+    <h2 class="my-4">{{ title }}</h2>
+    <ViewsRecomFull
+        class="mb-6"
+        :id="content.id" 
+        :image="content.image" 
+        :title="content['name_' + $i18n.locale]"
+        :description="content['description_' + $i18n.locale]"
+        :subtitleTop="content['company_' + $i18n.locale]"
+        :subtitleBottom="content['available_on_' + $i18n.locale]"
+        :features="content.Feature" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      content: {
+        type: Object,
+        required: true
+      },
+    }
+  }
+
+</script>
