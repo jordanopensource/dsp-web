d43bfece23629ccd7668890bbbad1a63489831d2
Wissam <wissam_ataleh@outlook.com>
2021-02-04 19:15:15 +0200

    Update layout of Helpdesk component

--- components/Helpdesk/index.vue
+++ components/Helpdesk/index.vue
@@ -2,10 +2,8 @@
   <div class="container">
     <h2 class="my-4">{{ $t('helpdesk') }}</h2>
     <p class="mb-4 lg:w-3/4">{{ helpdeskPage['description_' + $i18n.locale] }}</p>
-    <div class="flex flex-col md:flex-row flex-no-wrap my-10">
-      <FormsOTSForm class="max-w-md"/>
-      <HelpdeskOthersList v-if="helpdesksList.length" :contentList="helpdesksList" :title="$t('allHelpdesks')" class="my-10 md:my-0 md:ltr:ml-20 lg:ltr:ml-40 md:rtl:mr-20 lg:rtl:mr-40" />
-    </div>
+    <FormsOTSForm class="max-w-md my-10" />
+    <HelpdeskOthersList v-if="helpdesksList.length" :contentList="helpdesksList" :title="$t('allHelpdesks')" class="my-10" />
   </div>
 </template>
 
