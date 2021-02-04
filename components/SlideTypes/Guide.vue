a8c7a3d541db6e4d71364c76a0073b383ccff55a
Wissam <wissam_ataleh@outlook.com>
2021-02-04 18:14:31 +0200

    Limit slide to 2 items per slide

--- components/SlideTypes/Guide.vue
+++ components/SlideTypes/Guide.vue
@@ -3,7 +3,8 @@
     <h1 class="">{{ slide['title_' + $i18n.locale] ? slide['title_' + $i18n.locale] : ''}}</h1>
     <h4 class="lg:w-3/4 my-4">{{ slide['description_' + $i18n.locale] ? slide['description_' + $i18n.locale]: '' }}</h4>
     <div v-if="slide.guides.length" class="flex flex-wrap flex-row">
-      <div v-for="item in orderBy(slide.guides, 'published_at', -1)" :key="item.id" class="ltr:mr-10 rtl:ml-10 my-4">
+      <div v-for="item in limitBy(orderBy(slide.guides, 'published_at', -1), 2)" :key="item.id"
+        class="ltr:mr-10 rtl:ml-10 my-4">
         <NuxtLink :to="localePath('/digital-safety/' + item.id)" class="flex flex-row flex-no-wrap items-center">
           <UIImage v-if="item.icon" :image="item.icon" size="small" class="icon inline-block" />
           <img v-else src="/images/placeholder.png" alt="Placeholder" class="icon inline-block" />
