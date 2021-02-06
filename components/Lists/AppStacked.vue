f555828797121bf6d0b157179c09ef3bfa981f72
Wissam <wissam_ataleh@outlook.com>
2021-02-06 19:13:23 +0200

    Add component Lists/AppStacked

---
+++ components/Lists/AppStacked.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="stacked-list container">
+    <h2 v-if="title" class="my-4">{{ title }}</h2>
+    <p v-if="!contentList.length">{{ $t('noResults') }}</p>
+    <ViewsAppFull
+        v-for="item in contentList"
+        :key="item.id"
+        :id="item.id" 
+        :image="item.image" 
+        :name="item['name_' + $i18n.locale]"
+        :description="item['description_' + $i18n.locale]"
+        :publisher="item.app_publisher"
+        :website="item.website"
+        :platforms="item.Platform"
+        class="mb-10 p-4 bg-white" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String
+      },
+      contentList: {
+        type: Array,
+        required: true
+      },
+    }
+  }
+
+</script>
