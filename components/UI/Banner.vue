acab46639be67c9d2da31274e7026877ee562848
Wissam <wissam_ataleh@outlook.com>
2021-02-06 19:11:35 +0200

    Add conditional statement to banner description

--- components/UI/Banner.vue
+++ components/UI/Banner.vue
@@ -2,7 +2,7 @@
   <div id="banner" class="bg-white">
     <div class="container py-10">
       <h1>{{ title }}</h1>
-      <p class="my-4 max-w-3xl">{{ description }}</p>
+      <p v-if="description" class="my-4 max-w-3xl">{{ description }}</p>
     </div>
   </div>
 </template>
