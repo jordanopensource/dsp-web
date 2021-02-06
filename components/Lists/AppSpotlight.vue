e51a5144af46d15e3ab5b4d3303a99d4078880d0
Wissam <wissam_ataleh@outlook.com>
2021-02-06 19:33:48 +0200

    Add component Lists/AppSpotlight to homepage

---
+++ components/Lists/AppSpotlight.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="spotlight-list container mb-12">
+    <h2 class="mb-8">{{ title }}</h2>
+    <ViewsAppFull
+      :id="content.id" 
+      :image="content.image" 
+      :name="content['name_' + $i18n.locale]"
+      :description="content['description_' + $i18n.locale]"
+      :publisher="content.app_publisher"
+      :website="content.website"
+      :platforms="content.Platform" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String
+      },
+      content: {
+        type: Object,
+        required: true
+      },
+    }
+  }
+
+</script>
