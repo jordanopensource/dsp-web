a1812572d4b7a3d25628ba87f35f71a902171b4f
Wissam <wissam_ataleh@outlook.com>
2021-02-06 21:50:40 +0200

    Increase spacing between navbar items

--- components/UI/Navbar.vue
+++ components/UI/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <NuxtLink v-for="item in menu.Menu" :key="item.id" :to="ifSlash(item.path) ? localePath(item.path) : localePath('/' + item.path)"
-      class="block lg:inline-block py-2 lg:py-0 px-0 lg:ltr:pl-4 lg:rtl:pr-4">
+      class="block lg:inline-block py-2 lg:py-0 px-0 lg:ltr:pl-8 lg:rtl:pr-8">
       <h4>{{ item['name_' + $i18n.locale] }}</h4>
     </NuxtLink>
     <UILanguageSwitcher />
