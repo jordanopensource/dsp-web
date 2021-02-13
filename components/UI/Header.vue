18a20e3ff7f3d42b65a59d966789806f31faa935
Wissam <wissam_ataleh@outlook.com>
2021-02-13 18:22:20 +0200

    Update menu button style

--- components/UI/Header.vue
+++ components/UI/Header.vue
@@ -10,21 +10,19 @@
               alt="Jordan Open Source Association Logo">
           </NuxtLink>
         </div>
+        <UINavbar v-if="!isOpen" class="w-full hidden lg:block ltr:text-right rtl:text-left flex-grow mt-4 m-0" />
+
         <div class="block lg:hidden">
-          <button @click="isOpen = !isOpen" class="flex items-end">
-            <svg class="svg-inline--fa menu-icon" aria-hidden="true" focusable="false" data-prefix="fas" role="img"
-              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path v-if="!isOpen" fill="currentColor"
-                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
-              </path>
-              <path v-if="isOpen" fill="currentColor"
-                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z">
-              </path>
-            </svg>
+          <button @click="isOpen = !isOpen" class="hamburger hamburger--spin" :class="isOpen ? 'is-active' : ''"
+            type="button">
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
           </button>
         </div>
-        <UINavbar :class="isOpen ? 'block' : 'hidden'"
-          class="w-full lg:block lg:ltr:text-right lg:rtl:text-left flex-grow mt-4 lg:m-0" />
+        <transition name="fade">
+          <UINavbar v-if="isOpen" class="w-full block flex-grow mt-4" />
+        </transition>
       </div>
     </div>
   </header>
