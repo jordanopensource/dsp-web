fb6d0d47aa3c2b9c46158637837a768ae7ac879d
itsmohmans <mohmans@pm.me>
2022-09-28 17:50:17 +0300

    Add PublisherHeader component

--- pages/publisher/_id/index.vue
+++ pages/publisher/_id/index.vue
@@ -3,22 +3,7 @@
     <p v-if="$fetchState.pending">...</p>
     <div v-else>
       <article v-if="publisher" class="container py-12">
-        <div class="flex flex-col sm:flex-row flex-nowrap mb-12 items-center justify-start">
-          <UIImage 
-            v-if="publisher.Image" 
-            :image="publisher.Image"
-            size="medium"
-            class="app-image" />
-          <div class="mt-4 sm:mt-0 sm:ltr:ml-8 sm:rtl:mr-8">
-            <h1 id="publisher-name">
-              {{publisher["title_" + $i18n.locale]}}
-            </h1>
-          </div>
-        </div>
-        <p v-if="publisher['description_' + $i18n.locale]"
-          class="py-8">
-          {{publisher["description_" + $i18n.locale]}}
-        </p>
+        <ViewsPublisherHeader :publisher="publisher"/>
         <ListsAppAll 
           v-if="publisher.apps.length"
           :title="$t('appsBy') + ' ' + publisher['title_' + $i18n.locale]"
@@ -31,63 +16,47 @@
 
 <script>
 export default {
-    data() {
-      return {
-        publisher: null,
+  data() {
+    return {
+      publisher: null,
+    };
+  },
+  async fetch() {
+    let publisher = await this.$store.state.apps.list.find((obj) => {
+      return obj.id == this.$route.params.id;
+    });
+    try {
+      if (publisher && publisher.id == this.$route.params.id) {
+        this.publisher = publisher;
+        return publisher;
       }
-    },
-    async fetch() {
-      let publisher = await this.$store.state.apps.list.find((obj) => {
-        return obj.id == this.$route.params.id;
-      })
-      try {
+      else if (!publisher) {
+        await this.$store.dispatch("apps/fetchPublishers");
+        publisher = await this.$store.state.apps.publishers.find((obj) => {
+          return obj.id == this.$route.params.id;
+        });
         if (publisher && publisher.id == this.$route.params.id) {
-          this.publisher = publisher
-          return publisher
-        } else if (!publisher) {
-          await this.$store.dispatch("apps/fetchPublishers");
-          publisher = await this.$store.state.apps.publishers.find((obj) => {
-            return obj.id == this.$route.params.id;
-          })
-          if (publisher && publisher.id == this.$route.params.id) {
-            this.publisher = publisher
-            return publisher
-          } else {
-            throw 404
-          }
-        } else {
-          throw 404
+          this.publisher = publisher;
+          return publisher;
         }
-      } catch (err) {
-        return this.$nuxt.error({
-          statusCode: 404,
-          message: '404 Page Not Found'
-        })
+        else {
+          throw 404;
+        }
+      }
+      else {
+        throw 404;
       }
     }
+    catch (err) {
+      return this.$nuxt.error({
+        statusCode: 404,
+        message: "404 Page Not Found"
+      });
+    }
+  },
 }
 
 </script>
 
 <style scoped lang="postcss">
-  .app-image {
-    width: 124px;
-    height: 124px;
-    object-fit: cover;
-    object-position: 50% 50%;
-    @apply rounded-2xl;
-  }
-  @screen sm {
-    .app-image {
-      width: 140px;
-      height: 140px;
-    }
-  }
-
-  @screen lg {
-    .app-image {
-      width: 160px;
-      height: 160px;
-    }
-  }
 </style>
