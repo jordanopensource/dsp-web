014b36ae32de16a75ac7526dc2889867d820a487
thread-koder <mohamad.fberjawi@gmail.com>
2023-02-02 16:31:25 +0300

    Fix the publisher page

--- pages/publisher/_id/index.vue
+++ pages/publisher/_id/index.vue
@@ -3,12 +3,13 @@
     <p v-if="$fetchState.pending">...</p>
     <div v-else>
       <article v-if="publisher" class="container py-12">
-        <ViewsPublisherHeader :publisher="publisher"/>
-        <ListsAppAll 
+        <ViewsPublisherHeader :publisher="publisher" />
+        <ListsAppAll
           v-if="publisher.apps.length"
           :title="$t('appsBy') + ' ' + publisher['title_' + $i18n.locale]"
           :contentList="publisher.apps"
-          class="my-10" />
+          class="my-10"
+        />
       </article>
     </div>
   </div>
@@ -22,42 +23,19 @@ export default {
     };
   },
   async fetch() {
-    let publisher = await this.$store.state.apps.list.find((obj) => {
-      return obj.id == this.$route.params.id;
-    });
     try {
-      if (publisher && publisher.id == this.$route.params.id) {
-        this.publisher = publisher;
-        return publisher;
-      }
-      else if (!publisher) {
-        await this.$store.dispatch("apps/fetchPublishers");
-        publisher = await this.$store.state.apps.publishers.find((obj) => {
-          return obj.id == this.$route.params.id;
-        });
-        if (publisher && publisher.id == this.$route.params.id) {
-          this.publisher = publisher;
-          return publisher;
-        }
-        // TODO: Fix publlisher page sometimes throws 404 when loaded from back / a nuxt link
-        else {
-          throw 404;
-        }
-      }
-      else {
-        throw 404;
-      }
-    }
-    catch (err) {
+      const response = await this.$axios.get(
+        `/api/app-publishers/${this.$route.params.id}`
+      );
+      this.publisher = response.data;
+    } catch (err) {
       return this.$nuxt.error({
         statusCode: 404,
-        message: "404 Page Not Found"
+        message: "404 Page Not Found",
       });
     }
   },
-}
-
+};
 </script>
 
-<style scoped lang="postcss">
-</style>
+<style scoped lang="postcss"></style>
